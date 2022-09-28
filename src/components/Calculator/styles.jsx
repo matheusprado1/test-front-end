ad3650eed7804d7d973a4ac26ea5d48b8fccc098
matheusprado1 <matheusprado1096@gmail.com>
2022-09-28 17:53:45 -0300

    feat: update style Calculator

--- src/components/Calculator/styles.jsx
+++ src/components/Calculator/styles.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  .container {
+  .main_container {
     display: flex;
-    /* flex-wrap: nowrap; */
-    border: 2px solid #e6e6e6;
+    flex-wrap: nowrap;
+    border: 1px solid #dde6e9;
   }
   .title {
-    margin-top: 25%;
+    margin-top: 12%;
     padding: 5%;
     font-size: 22px;
     color: #333333;
@@ -15,8 +15,8 @@ export const StyledContainer = styled.div`
 
   .subtitle {
     font-style: italic;
-    border-bottom: 1px solid blue;
-    color: blue;
+    border-bottom: 1px solid #5d9cec;
+    color: #3d75bb;
   }
 
   .result {
@@ -24,6 +24,16 @@ export const StyledContainer = styled.div`
     align-items: center;
     flex-direction: column;
     padding: 23% 0%;
-    background-color: #fff;
+    background-color: rgba(209, 220, 227, 0.18)
+  }
+
+  .column {
+    padding: 0 10%;
+    width: 25vw;
+    background: #fff;
+  }
+
+  .column2 {
+    width: 20vw;
   }
 `;
