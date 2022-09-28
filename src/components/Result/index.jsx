a32230a6744cc24bfbe1b67977171bbf0faea99f
matheusprado1 <matheusprado1096@gmail.com>
2022-09-28 13:57:04 -0300

    feat: refactoring component Result(props)

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,18 +1,11 @@
 import { StyledContainer } from "./styles";
 
-const Result = () => {
+const Result = ({ days, value }) => {
   return (
     <StyledContainer>
       <div className="container">
-        <h2 className="title">Você receberá:</h2>
-        <p className="text">Amanhã:</p>
-        <strong className="strong">R$ 0,00</strong>
-        <p className="text">Em 15 dias:</p>
-        <strong className="strong">R$ 0,00</strong>
-        <p className="text">Em 30 dias:</p>
-        <strong className="strong">R$ 0,00</strong>
-        <p className="text">Em 90 dias:</p>
-        <strong className="strong">R$ 0,00</strong>
+        <p className="text">{days}</p>
+        <strong className="strong">R$ {value}</strong>
       </div>
     </StyledContainer>
   );
