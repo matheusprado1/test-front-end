7e8e6740d23b69b8f07d3da599778ef867ee3717
matheusprado1 <matheusprado1096@gmail.com>
2022-09-29 13:12:37 -0300

    feat: BrowserRouter and Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import Provider from "./provider";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
