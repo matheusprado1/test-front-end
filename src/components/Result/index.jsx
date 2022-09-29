a119e94980dcfab40a10795f850e6586aa62d5b9
matheusprado1 <matheusprado1096@gmail.com>
2022-09-29 15:14:15 -0300

    feat: refactoring component Result with calculation and style changes

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,13 +1,41 @@
+import { useContext } from "react";
+import { ApiContext } from "../../provider/ApiProvider/index.jsx";
 import { StyledContainer } from "./styles";
 
-const Result = ({ days, value }) => {
+const Result = () => {
+  const { calculation } = useContext(ApiContext);
+
   return (
     <StyledContainer>
-      <div>
-        <p className="text">
-          {days}: <strong>R$ {value}</strong>
-        </p>
-      </div>
+      <>
+        <div className="result">
+          <h2 className="result_title">VOCÊ RECEBERÁ:</h2>
+        </div>
+        <div className="result_container">
+          <span className="days">Amanhã:</span>
+          <span className="total">
+            {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[1]},00`}
+          </span>
+        </div>
+        <div className="result_container">
+          <span className="days">Em 15 dias:</span>
+          <span className="total">
+            {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[15]},00`}
+          </span>
+        </div>
+        <div className="result_container">
+          <span className="days">Em 30 dias:</span>
+          <span className="total">
+            {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[30]},00`}
+          </span>
+        </div>
+        <div className="result_container">
+          <span className="days">Em 90 dias:</span>
+          <span className="total">
+            {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[90]},00`}
+          </span>
+        </div>
+      </>
     </StyledContainer>
   );
 };
--- src/components/Result/styles.jsx
+++ src/components/Result/styles.jsx
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  .text {
+
+padding: 70px 90px;
+
+  .result_container {
+    line-height: 1.8;
     font-style: italic;
     color: #5d9cec;
   }
+
+  .result_title {
+    color: #5d9cec;
+    border-bottom: 1px solid #5d9cec;
+    font-size: 16px;
+  }
+
 `;
