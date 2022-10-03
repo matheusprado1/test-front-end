1a54ab84cefa1ec61b2a30cc1679da3120836f1a
matheusprado1 <matheusprado1096@gmail.com>
2022-10-03 13:36:30 -0300

    feat: added * in days

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -12,25 +12,25 @@ const Result = () => {
           <h2 className="result_title">VOCÊ RECEBERÁ:</h2>
         </div>
         <div className="result_container">
-          <span className="days">Amanhã:</span>
+          <span className="days">Amanhã: </span>
           <span className="total">
             {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[1]},00`}
           </span>
         </div>
         <div className="result_container">
-          <span className="days">Em 15 dias:</span>
+          <span className="days">Em 15 dias: </span>
           <span className="total">
             {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[15]},00`}
           </span>
         </div>
         <div className="result_container">
-          <span className="days">Em 30 dias:</span>
+          <span className="days">Em 30 dias: </span>
           <span className="total">
             {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[30]},00`}
           </span>
         </div>
         <div className="result_container">
-          <span className="days">Em 90 dias:</span>
+          <span className="days">Em 90 dias: </span>
           <span className="total">
             {calculation.length === 0 ? "R$ 0,00" : `R$ ${calculation[90]},00`}
           </span>
