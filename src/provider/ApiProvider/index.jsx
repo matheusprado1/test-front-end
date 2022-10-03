3c8a0209b9cc7c7db04afdfca3516534969ba46a
matheusprado1 <matheusprado1096@gmail.com>
2022-10-03 13:15:00 -0300

    feat: API error handling

--- src/provider/ApiProvider/index.jsx
+++ src/provider/ApiProvider/index.jsx
@@ -6,6 +6,9 @@ export const ApiContext = createContext();
 
 const ApiProvider = ({ children }) => {
   const [calculation, setCalculation] = useState([]);
+  const [time, setTime] = useState(false);
+  const [error, setError] = useState(false);
+  const [delay, setDelay] = useState(false);
 
   const insertCalculation = (data) => {
     api
@@ -14,18 +17,50 @@ const ApiProvider = ({ children }) => {
         setCalculation(response.data);
       })
       .catch((err) => {
-        toast("Ocorreu um erro.", {
-          className: "toastify-color-progress-error",
-        });
+        toast.error("Ocorreu um erro, tente novamente.", {});
       });
+  };
+
+  const testDelay = (data) => {
+    setTimeout(() => {
+      toast.warn("Conexão lenta, por favor aguarde!", {});
+    });
+    api.post("?delay=5000", data).then((response) => {
+      setCalculation(response.data);
+    });
+  };
+
+  const testTime = (data) => {
+    api.post("?timeout", data).catch((err) => {
+      setTimeout(() => {
+        toast.error("Tempo esgotado, tente novamente!", {});
+      }, 2000);
+    });
+  };
+
+  const testError = (data) => {
+    api.post("?internalError", data).catch((err) => {
+      setTime(() => {
+        toast.error("Erro de conexão, tente novamente!", {});
+      }, 2000);
+    });
   };
 
   return (
     <ApiContext.Provider
       value={{
-        calculation,
-        setCalculation,
         insertCalculation,
+        calculation,
+        delay,
+        time,
+        error,
+        setCalculation,
+        setDelay,
+        setError,
+        setTime,
+        testError,
+        testTime,
+        testDelay,
       }}
     >
       {children}
